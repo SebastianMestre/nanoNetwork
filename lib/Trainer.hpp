2f21c9f71eefb4e58969b987147067ee04fbc193
SebastianMestre <mestresebastianunr@gmail.com>
2018-11-05 11:52:55 -0300

    Implements Stochastic Gradient Descent

--- lib/Trainer.hpp
+++ lib/Trainer.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <vector>
+#include <cassert>
+#include <cstdio>
 
 #include "NeuralNetwork.hpp"
 #include "NeuralNetworkLayer.hpp"
@@ -13,96 +15,146 @@ namespace nanoNet {
     };
 
 
-    // Implementacion del patron de diseno "Visitor"
     class Trainer {
-        // TODO: settings
 
-        std::size_t epoch_count;
-        std::size_t batch_size;
-        float learning_rate;
+        using vector_type = NeuralNetworkLayer::vector_type;
+        using matrix_type = NeuralNetworkLayer::matrix_type;
+    public:
+        std::size_t epoch_count = 100;
+        std::size_t batch_size = 30;
+        float learning_rate = 0.01f;
 
     public:
+
         using DataSet = std::vector<DataPoint>;
-        Trainer () {}
 
-        void train (
-            NeuralNetwork& network,
-            DataSet dataSet
-        ) const {
-
-            if (network.layerCount() == 0)
-                return;
-
-            for (std::size_t iEpoch = 0; iEpoch < epoch_count; iEpoch++) {
-
-                std::shuffle(dataSet.begin(), dataSet.end(), std::default_random_engine(0));
-
-                for (std::size_t iDatum = 0; iDatum < dataSet.size();) {
-                    for (std::size_t iBatch = 0; iBatch < batch_size && iDatum < dataSet.size(); ++iBatch, ++iDatum) {
-
-                        std::vector<NeuralNetworkLayer::vector_type> results;
-
-                        for (const auto& layer : network.m_layers)
-                                results.push_back( layer.predict(
-                                    results.empty()
-                                        ? dataSet[iDatum].input
-                                        : results.back()));
-
-                        std::vector<NeuralNetworkLayer::vector_type> gradients(network.layerCount());
-
-                        gradients.back() = gradientFromExample(
-                            network.m_layers.back(),
-                            results.back(),
-                            dataSet[iDatum].output
-                        );
-
-                        for(int i = network.layerCount()-2; i >= 1; i--){
-                            gradients[i] = gradientFromNext(
-                                network.m_layers[i],
-                                network.m_layers[i+1],
-                                results[i+1],
-                                gradients[i+1]
-                            );
-
-                            // network.hiddenLayers[i].gradientFromActives(network.hiddenLayers[i-1]);
-                        }
-
-                        // network.hiddenLayers[0].gradientFromActives(trainData[iDatum].input);
-
-                    }
-
-                    // for (auto& layer : network.hiddenLayers)
-                        // layer.applyTraining(learningRate, batchSize);
-                }
+        void train ( NeuralNetwork& network, DataSet dataSet ) {
+            auto rd = std::random_device{};
+            for (size_t i = 0; i < epoch_count; ++i) {
+                std::shuffle( dataSet.begin(), dataSet.end(), rd );
+                train_once( network, { dataSet.begin(), dataSet.begin() + batch_size });
             }
         }
 
-        static NeuralNetworkLayer::vector_type gradientFromExample (
-            const NeuralNetworkLayer& layer,
-            const NeuralNetworkLayer::vector_type& values,
-            const NeuralNetworkLayer::vector_type& example
-        ) {
-            NeuralNetworkLayer::vector_type gradient ( layer.outputCount(), 0.0f );
+        void train_once ( NeuralNetwork& network, const DataSet& dataSet ) {
+            using std::size_t;
+            using std::puts;
 
-            for (std::size_t i = 0; i < layer.outputCount(); ++i)
+            std::vector <vector_type> bias_gradient_accum {network.layerCount()};
+            for (size_t i = 0; i < bias_gradient_accum.size(); ++i)
+                bias_gradient_accum[i].resize( network.m_layers[i].outputCount() , 0.0 );
+
+            std::vector <matrix_type> weight_gradient_accum {network.layerCount()};
+            for (size_t i = 0; i < weight_gradient_accum.size(); ++i)
+                weight_gradient_accum[i].resize( network.m_layers[i].outputCount() );
+
+            for (size_t i = 0; i < weight_gradient_accum.size(); ++i)
+                for (size_t j = 0; j < network.m_layers[i].outputCount(); ++j )
+                    weight_gradient_accum[i][j].resize( network.m_layers[i].m_weights[j].size(), 0.0 );
+
+
+            std::vector <vector_type> value ( network.layerCount() );
+            std::vector <vector_type> gradient ( network.layerCount() );
+            std::vector <vector_type> bias_gradient ( network.layerCount() );
+            std::vector <matrix_type> weight_gradient ( network.layerCount() );
+
+            for ( auto& dataPoint : dataSet ) {
+
+                for (size_t i = 0; i < network.layerCount(); ++i)
+                    value[i] = network.m_layers[i].predict( i == 0 ? dataPoint.input : value[i-1]);
+
+
+                gradient.back() = gradientFromExample( value.back(), dataPoint.output );
+                for (size_t i = network.layerCount()-1; i--;)
+                    gradient[i] = gradientFromNext ( network.m_layers[i] , network.m_layers[i+1], value[i+1], gradient[i+1]);
+
+
+                for (size_t i = 0; i < network.layerCount(); ++i)
+                    bias_gradient[i] = biasGradientFromValues(network.m_layers[i], value[i], gradient[i]);
+
+
+                weight_gradient[0] = weightsGradientFromPrevious(bias_gradient[0], dataPoint.input);
+                for (size_t i = 1; i < network.layerCount(); ++i)
+                    weight_gradient[i] = weightsGradientFromPrevious(bias_gradient[i], value[i-1]);
+
+
+                for(size_t i = 0; i < network.layerCount(); ++i) {
+
+                    for (size_t j = 0; j < bias_gradient[i].size(); ++j)
+                        bias_gradient_accum[i][j] += bias_gradient[i][j];
+
+                    for (size_t j = 0; j < weight_gradient[i].size(); ++j)
+                        for (size_t k = 0; k < weight_gradient[i][j].size(); ++k )
+                            weight_gradient_accum[i][j][k] += weight_gradient[i][j][k];
+
+                }
+            }
+
+            auto coeff = learning_rate / dataSet.size();
+            for (size_t i = 0; i < network.layerCount(); ++i) {
+                for (size_t j = 0; j < bias_gradient_accum[i].size(); ++j)
+                    network.m_layers[i].m_biases[j] -= bias_gradient_accum[i][j] * coeff;
+
+                for (size_t j = 0; j < weight_gradient_accum[i].size(); ++j)
+                    for (size_t k = 0; k < weight_gradient_accum[i][j].size(); ++k)
+                        network.m_layers[i].m_weights[j][k] -= weight_gradient_accum[i][j][k] * coeff;
+
+            }
+        }
+
+    private:
+
+        static vector_type gradientFromExample (
+            const vector_type& values,
+            const vector_type& example
+        ) {
+            vector_type gradient ( values.size(), 0.0f );
+
+            for (std::size_t i = 0; i < values.size(); ++i)
                 gradient[i] = 2 * ( values[i] - example[i] );
 
             return gradient;
         }
 
-        static NeuralNetworkLayer::vector_type gradientFromNext (
+        static vector_type gradientFromNext (
             const NeuralNetworkLayer& layer,
             const NeuralNetworkLayer& next,
-            const NeuralNetworkLayer::vector_type& next_values,
-            const NeuralNetworkLayer::vector_type& next_gradient
+            const vector_type& next_values,
+            const vector_type& next_gradient
         ) {
-            NeuralNetworkLayer::vector_type gradient ( layer.outputCount(), 0.0f );
+            vector_type gradient ( layer.outputCount(), 0.0f );
 
             for (std::size_t i = 0; i < next.outputCount(); ++i)
                 for (std::size_t j = 0; j < layer.outputCount(); ++j)
                     gradient[j] += next_gradient[i] * next.m_activationFunction[ next_values[i] ] * next.weights()[i][j];
 
             return gradient;
+        }
+
+        static vector_type biasGradientFromValues (
+            const NeuralNetworkLayer& layer,
+            const vector_type& values,
+            const vector_type& gradients
+        ) {
+            vector_type bias_gradients (layer.outputCount(), 0.0f);
+
+            for (std::size_t i = 0; i < layer.outputCount(); ++i)
+                bias_gradients[i] = gradients[i] * layer.m_activationFunction[ values[i] ];
+
+            return bias_gradients;
+        }
+
+        static matrix_type weightsGradientFromPrevious (
+            const vector_type& bias_gradients,
+            const vector_type& previous_values
+        ) {
+            matrix_type weights_gradients (bias_gradients.size(), vector_type( previous_values.size(), 0.0f ));
+
+            for(std::size_t i = 0; i < bias_gradients.size(); ++i)
+                for (std::size_t j = 0; j < previous_values.size(); ++j)
+                    weights_gradients[i][j] += bias_gradients[i] * previous_values[j];
+
+            return weights_gradients;
         }
 
     };
