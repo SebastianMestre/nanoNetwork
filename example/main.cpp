e5499a6a8a6c0f61f12e07fe552c222caad53175
SebastianMestre <mestresebastianunr@gmail.com>
2018-11-05 11:53:06 -0300

    Updates example

--- example/main.cpp
+++ example/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <random>
 #include "../lib/NeuralNetwork.hpp"
 #include "../lib/Trainer.hpp"
 
@@ -6,11 +8,33 @@ using namespace nanoNet;
 
 int main() {
 
-    NeuralNetwork pepe;
+    NeuralNetwork network;
 
-    pepe.addLayer({256, 16, {ActivationFunction::Which::Linear}});
-    pepe.addLayer({16, 16, {ActivationFunction::Which::Linear}});
-    pepe.addLayer({16, 10, {ActivationFunction::Which::Linear}});
+    network.addLayer({2, 8, {ActivationFunction::Which::Relu}});
+    network.addLayer({8, 1, {ActivationFunction::Which::Linear}});
+
+    // 10000 epochs, batch size = 2, training rate = 0.002
+    Trainer trainer { 10000 , 2 , 0.002 };
+
+    Trainer::DataSet data {{
+        {{{0.0, 0.0}},{{0.0}}},
+        {{{0.0, 1.0}},{{1.0}}},
+        {{{1.0, 0.0}},{{1.0}}},
+        {{{1.0, 1.0}},{{0.0}}}
+    }};
+
+    // stochastic gradient descent
+    trainer.train ( network, data );
+
+    auto rnd_base = []( float k ){ return [=]( float x ){ return ( int(x*k+0.5) ) / k; }; };
+    auto rnd = rnd_base(2);
+
+    std::cout << '\n';
+    std::cout << rnd(network.predict({{0.0, 0.0}})[0]) << '\n';
+    std::cout << rnd(network.predict({{0.0, 1.0}})[0]) << '\n';
+    std::cout << rnd(network.predict({{1.0, 0.0}})[0]) << '\n';
+    std::cout << rnd(network.predict({{1.0, 1.0}})[0]) << '\n';
+    std::cout << '\n';
 
     return 0;
 }
